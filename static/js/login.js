9a56702894319e8a355760d807f5bb96a729d6d9
DevinParentice <dude114100@gmail.com>
2020-10-29 17:02:58 -0400

    Made links between forms work

--- static/js/login.js
+++ static/js/login.js
@@ -10,12 +10,26 @@ $(document).ready(function () {
 	var signupClose = document.getElementById("signupClose");
 	var loginClose = document.getElementById("loginClose");
 
+	var showSignup = document.getElementById("showSignup");
+	var showLogin = document.getElementById("showLogin");
+
 	// When the user clicks on the button, open the modal
 	loginBtn.onclick = function () {
 		loginModal.style.display = "block";
 	}
 	signupBtn.onclick = function () {
 		signupModal.style.display = "block";
+	}
+
+	showSignup.onclick = function () {
+		loginModal.style.display = "none";
+		signupModal.style.display = "block";
+	}
+
+	showLogin.onclick = function () {
+		signupModal.style.display = "none";
+		loginModal.style.display = "block";
+
 	}
 
 	// When the user clicks on <span> (x), close the modal
