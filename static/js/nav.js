a4873b308e25a890ec70970ff70051ef46252af6
DevinParentice <dude114100@gmail.com>
2020-10-29 00:56:08 -0400

    Added navbar

---
+++ static/js/nav.js
@@ -0,0 +1,9 @@
+$(function () {
+	$(".nav-toggle").on("click", function () {
+		if ($(".nav-item").hasClass("active")) {
+			$(".nav-item").removeClass("active");
+		} else {
+			$(".nav-item").addClass("active");
+		}
+	});
+});
